4bfd160b18901417ce83376e92189b148ac9ea58
Ivana Kaz <kazivana@gmail.com>
2020-12-13 11:53:08 +0100

    package initialization, adding functions and data

---
+++ R/data.R
@@ -0,0 +1,100 @@
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Historical simulation using the ALADIN regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"historical_ALADIN"
+
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Historical simulation using the RCA regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"historical_RCA"
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Future simulation (RCP26) using the ALADIN regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"rcp26_ALADIN"
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Future simulation (RCP45) using the ALADIN regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"rcp45_ALADIN"
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Future simulation (RCP45) using the RCA regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"rcp45_RCA"
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Future simulation (RCP85) using the ALADIN regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"rcp85_ALADIN"
+
+
+#' Simulated Hourly Precipitation, CNRM-CERFACS-CNRM-CM5 global climate model
+#'
+#' Future simulation (RCP85) using the RCA regional model
+#'
+#' @format data.table with
+#' \describe{
+#'   \item{date}{date and time}
+#'   \item{p}{precipitation in kg/s}
+#'   \item{forcing}{forcing string id}
+#'   ...
+#' }
+"rcp85_RCA"
+
+
+
